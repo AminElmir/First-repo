4d0e708bfb42b0efe44ae5dc3accc4f8a2518ad9
AminElmir! <elmirmohamedamine@gmail.com>
2024-08-11 18:49:47 +0100

    hab

--- firstfolder/amine1.js
+++ firstfolder/amine1.js
@@ -11,5 +11,17 @@ function guess(n){
     var x = 1
     var y= 2
    return ( Math.floor(Math.random()*n+1))
-
-}
+   //function 1
+   function number(a,b) {
+    if (a>b){
+        return a
+    }
+    return b
+   }
+   //function age
+   function pass(pas){
+    if (pas>=8) {
+        return "ok"
+    }
+    return "no"
+   }
